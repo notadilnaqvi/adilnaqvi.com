3c657c5779383d9e1182ace5bb57404b57a60465
Adil Naqvi <hello@adilnaqvi.com>
2022-01-02 20:01:51 +0500

    feat: add next-pwa for offline fallback page

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
 	images: {
 		domains: ['dummyimage.com'],
 	},
-};
+	pwa: {
+		dest: 'public',
+		disable: process.env.NODE_ENV === 'development',
+	},
+	reactStrictMode: true,
+});
