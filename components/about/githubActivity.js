ae971c0232aa912e1763a3f0d63f1582560be418
Adil Naqvi <hello@adilnaqvi.com>
2022-02-14 22:33:39 +0500

    fix(about): change const to let
    rookie mistake

--- components/about/githubActivity.js
+++ components/about/githubActivity.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { calcTimeAgo, fetcher } from '@/utils';
 
 function GithubActivity() {
-	const { data, error } = useSWR('/api/github', fetcher);
+	let { data, error } = useSWR('/api/github', fetcher);
 
 	if (!data || error) {
 		return (
