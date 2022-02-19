111d55a3d3c1dbca088c3f3cc02d05518c584a3e
Adil Naqvi <hello@adilnaqvi.com>
2022-02-20 03:56:26 +0500

    feat(about): add multiple sections

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { motion } from 'framer-motion';
 
 import GithubActivity from '@/components/about/githubActivity';
@@ -19,22 +20,107 @@ const variants = {
 	},
 };
 
+const stagger = {
+	animate: { transition: { staggerChildren: 0.035 } },
+};
+
 function About() {
 	return (
-		<motion.section
+		<motion.div
+			variants={stagger}
 			initial='initial'
 			animate='animate'
-			variants={variants}
-			className='w-full pt-16'
+			className='flex flex-col pt-16 space-y-8'
 		>
-			<h1 className='text-2xl text-gray-700 mb-8'>What am I upto</h1>
-			<div className='flex flex-col space-y-2'>
-				<GithubActivity />
-				<TwitterActivity />
-				<SpotifyActivity />
-				<LichessActivity />
-			</div>
-		</motion.section>
+			<motion.section
+				variants={variants}
+				className='w-full'
+				id='what-am-i-upto'
+			>
+				<h1 className='text-2xl text-gray-700 mb-8'>What I'm upto</h1>
+				<div className='flex flex-col space-y-4'>
+					<GithubActivity />
+					<TwitterActivity />
+					<SpotifyActivity />
+					<LichessActivity />
+				</div>
+			</motion.section>
+			<motion.section
+				variants={variants}
+				className='w-full'
+				id='what-am-i-reading'
+			>
+				<h1 className='text-2xl text-gray-700 mb-8'>
+					What I'm reading
+				</h1>
+				<div className='flex space-y-4'>
+					<img
+						className='rounded-sm'
+						src='/assets/tgost.webp'
+						width='150'
+						height='200'
+						alt='Adil Naqvi'
+					/>
+				</div>
+			</motion.section>
+			<motion.section
+				variants={variants}
+				className='w-full'
+				id='what-am-i-watching'
+			>
+				<h1 className='text-2xl text-gray-700 mb-8'>
+					What I'm watching
+				</h1>
+				<div className='flex space-x-4'>
+					<img
+						className='rounded-sm'
+						src='/assets/al.webp'
+						width='150'
+						height='200'
+						alt='Adil Naqvi'
+					/>
+					<img
+						className='rounded-sm'
+						src='/assets/aot.webp'
+						width='150'
+						height='200'
+						alt='Adil Naqvi'
+					/>
+				</div>
+			</motion.section>
+			<motion.section
+				variants={variants}
+				className='w-full'
+				id='about-this-website'
+			>
+				<h1 className='text-2xl text-gray-700 mb-8'>
+					About this website
+				</h1>
+				<div className='flex flex-col space-y-4'>
+					<p className='text-gray-700'>
+						Made with Next.js and Tailwind
+					</p>
+					<p className='text-gray-700'>Hosted on Vercel</p>
+					<p className='text-gray-700'>
+						Open source under MIT license.&nbsp;
+						<a
+							className='lnk'
+							href='https://github.com/notadilnaqvi/adilnaqvi.com'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							See source code
+						</a>
+					</p>
+					<p className='text-gray-700'>
+						Have a suggestion?&nbsp;
+						<Link href='/contact'>
+							<a className='lnk'>Let me know</a>
+						</Link>
+					</p>
+				</div>
+			</motion.section>
+		</motion.div>
 	);
 }
 
