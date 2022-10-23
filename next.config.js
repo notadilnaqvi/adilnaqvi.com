a8c0b07d45f1b474f37a58e965f26ea0aaba9616
Adil Naqvi <notadilnaqvi@gmail.com>
2022-10-23 10:22:58 +0500

    feat: handle missing env vars, improve build messages
    - fail build if any required environments are missing
    - log Next config for debugging

--- next.config.js
+++ next.config.js
@@ -19,3 +19,29 @@ module.exports = withPWA({
 		];
 	},
 });
+
+// Fail build if any required environment variables are missing
+if (process.env.NODE_ENV === 'production') {
+  const requiredEnvVars = [
+    'GITHUB_PERSONAL_ACCESS_TOKEN',
+    'TWITTER_BEARER_TOKEN',
+    'SPOTIFY_REFRESH_TOKEN',
+    'SPOTIFY_CLIENT_ID',
+    'SPOTIFY_CLIENT_SECRET',
+    'LICHESS_PERSONAL_API_ACCESS_TOKEN',
+  ];
+  let missingEnvVarsStr = '';
+  for (const envVar of requiredEnvVars) {
+    if (typeof process.env[envVar] === 'undefined') {
+      missingEnvVarsStr += `- ${envVar}\n`;
+    }
+  }
+  if (missingEnvVarsStr) {
+    console.log(
+      `\u274c The following environment variables were not set. Please set them and try again.\n${missingEnvVarsStr}`,
+    );
+    process.exit(1);
+  } else {
+    console.log('\u2699\ufe0f next.config.js', JSON.stringify(module.exports, null, 2));
+  }
+}
