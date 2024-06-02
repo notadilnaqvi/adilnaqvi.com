e29732072855cb8c157fea78de91f0ca4f088b6a
Adil Naqvi <adil.naqvi@convertdigital.com.au>
2024-06-03 01:45:39 +0500

    Experimenting with Deno Cron jobs

--- server.ts
+++ server.ts
@@ -1,13 +1,14 @@
 import { serveDir } from "https://deno.land/std@0.224.0/http/file_server.ts";
 
-async function _updateIndexHtml() {
+async function updateIndexHtml() {
   const indexHtmlText = await Deno.readTextFile("index.html");
 
-  const updatedIndexHtmlText = indexHtmlText
-    .replace(
-      "{{ LICHESS_ACTIVITY }}",
-      'Played a <a href="#">bullet game</a> (3 hrs ago)',
-    )
+  const currentTime = new Date().toLocaleString();
+  
+  const updatedIndexHtmlText = indexHtmlText.replace(
+    "{{ LAST_UPDATED }}",
+    currentTime,
+  );
 
   await Deno.writeTextFile("index.html", updatedIndexHtmlText);
 }
@@ -16,4 +17,8 @@ Deno.serve(async (req) => {
   return await serveDir(req, {
     fsRoot: "./static/",
   });
+});
+
+Deno.cron("Update last updated", "*/10 * * * *", async () => {
+  await updateIndexHtml();
 });
