5d9974a8c9821224596462d9d00db13efc2ede74
Adil Naqvi <adil.naqvi@convertdigital.com.au>
2024-06-10 00:36:38 +0500

    Add offline support, update image and content

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("static-v1").then((cache) => {
+      cache.addAll(["index.html", "think.webp", "favicon.ico"]);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => response)
+      .catch(() => {
+        if (new URL(event.request.url).pathname === "/") {
+          return caches.match("index.html").then((cacheHit) => cacheHit);
+        }
+        return caches.match(event.request).then((cacheHit) => cacheHit);
+      }),
+  );
+});
